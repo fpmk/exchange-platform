e7998bd30fc83868218e362500c419dfa31832a6
fpmk.1 <fpmk1122@gmail.com>
2025-11-10 07:55:21 +0400

    refactor: trading page test

--- src/app/pages/trading/trading.spec.ts
+++ src/app/pages/trading/trading.spec.ts
@@ -1,17 +1,7 @@
 import { ComponentFixture } from '@angular/core/testing';
-import {
-  createMockAppStore,
-  createMockChartStore,
-  createMockExchangeWebsocketPort,
-  createMockExchangeWsStore,
-  createMockMarketDataPort,
-  createMockStoragePort,
-  createMockTradingPort,
-  createMockWsMarketDataPort
-} from '@exchange-platform/test-mocks';
-import { AppStore, ChartStore, ExchangeWebSocketStore } from '@exchange-platform/state';
+import { createMockAppStore } from '@exchange-platform/test-mocks';
+import { AppStore } from '@exchange-platform/state';
 import { WebsocketService } from '@exchange-platform/application-services';
-import { ExchangeWebsocketPort, MarketDataPort, StoragePort, TradingPort, WsMarketDataPort } from '@exchange-platform/ports';
 import { TradingPage } from './trading';
 import { MockBuilder, MockRender } from 'ng-mocks';
 import { Orderbook } from '@exchange-platform/orderbook';
@@ -25,7 +15,7 @@ const mockWSService = {
 describe('TradingPage', () => {
   let component: TradingPage;
   let fixture: ComponentFixture<TradingPage>;
-  let appStore: any;
+  let appStore: ReturnType<typeof createMockAppStore>;
 
   beforeEach(async () => {
     appStore = createMockAppStore();
@@ -33,41 +23,14 @@ describe('TradingPage', () => {
       .mock(Orderbook)
       .mock(ChartWidgetComponent)
       .mock(OrderForm)
+      .mock(WebsocketService)
       .provide({
         provide: AppStore,
         useValue: appStore,
       })
       .provide({
-        provide: ExchangeWebSocketStore,
-        useValue: createMockExchangeWsStore(),
-      })
-      .provide({
         provide: WebsocketService,
         useValue: mockWSService,
-      })
-      .provide({
-        provide: ExchangeWebsocketPort,
-        useValue: createMockExchangeWebsocketPort(),
-      })
-      .provide({
-        provide: ChartStore,
-        useValue: createMockChartStore(),
-      })
-      .provide({
-        provide: TradingPort,
-        useValue: createMockTradingPort(),
-      })
-      .provide({
-        provide: MarketDataPort,
-        useValue: createMockMarketDataPort(),
-      })
-      .provide({
-        provide: WsMarketDataPort,
-        useValue: createMockWsMarketDataPort(),
-      })
-      .provide({
-        provide: StoragePort,
-        useValue: createMockStoragePort(),
       });
     fixture = MockRender(TradingPage);
     component = fixture.componentInstance;
